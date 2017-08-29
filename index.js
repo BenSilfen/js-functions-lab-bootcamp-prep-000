c342d43f04fdde6fead9dad440a1c8e2c1b34f60
Benjamin Silfen <bfsilfen@gmail.com>
2017-08-29 18:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+<<<<<<< HEAD
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
@@ -9,4 +10,8 @@ function happyHolidayTo(holiday, name){
 }
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
+=======
+function happyHolidaysTo(name){
+  return ('Happy Holidays, ${name}!')
+>>>>>>> 71882d783f304bd78d61bbfa654b72b4249eb379
 }
